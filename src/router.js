96c8192c349cd8cea1cfbffdd4e02384da0bb753
Deepshi Sharma <deepshisharma123@gmail.com>
2023-07-17 10:52:42 +0530

    reverting changes for lazy loading the components

--- src/router.js
+++ src/router.js
@@ -1,30 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 // importing all the views components and registering them with the routes
 import MentorsList from './views/mentors/MentorsList.vue'
-import { defineAsyncComponent } from 'vue'
-// import MentorDetails from './views/mentors/MentorDetails.vue'
-// import ContactMentor from './views/requests/ContactMentor.vue'
-// import MentorRegisteration from './views/mentors/MentorRegisteration.vue'
-// import RequestsReceived from './views/requests/RequestsReceived.vue'
+// import { defineAsyncComponent } from 'vue'
+import MentorDetails from './views/mentors/MentorDetails.vue'
+import ContactMentor from './views/requests/ContactMentor.vue'
+import MentorRegisteration from './views/mentors/MentorRegisteration.vue'
+import RequestsReceived from './views/requests/RequestsReceived.vue'
 import NotFound from './views/NotFound.vue'
-import store from './store/index.js';
-// import UserAuth from './views/auth/UserAuth.vue'
+// import store from './store/index.js';
+import UserAuth from './views/auth/UserAuth.vue'
 // an optimisation that we import components only when needed
-const MentorDetails = defineAsyncComponent(() =>
-  import('./views/mentors/MentorDetails.vue')
-);
-const MentorRegisteration = defineAsyncComponent(() =>
-  import('./views/mentors/MentorRegisteration.vue')
-);
-const ContactMentor = defineAsyncComponent(() =>
-  import('./views/requests/ContactMentor.vue')
-);
-const RequestsReceived = defineAsyncComponent(() =>
-  import('./views/requests/RequestsReceived.vue')
-);
-const UserAuth = defineAsyncComponent(() =>
-  import('./views/auth/UserAuth.vue')
-);
+// const MentorDetails = defineAsyncComponent(() =>
+//   import('./views/mentors/MentorDetails.vue')
+// );
+// const MentorRegisteration = defineAsyncComponent(() =>
+//   import('./views/mentors/MentorRegisteration.vue')
+// );
+// const ContactMentor = defineAsyncComponent(() =>
+//   import('./views/requests/ContactMentor.vue')
+// );
+// const RequestsReceived = defineAsyncComponent(() =>
+//   import('./views/requests/RequestsReceived.vue')
+// );
+// const UserAuth = defineAsyncComponent(() =>
+//   import('./views/auth/UserAuth.vue')
+// );
 
 const router = createRouter({
   history: createWebHistory(),
